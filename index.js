687c54ab72a30a957bba692acbbc64c13369d068
Adarsh Dwivedi <152579045+adarshh8@users.noreply.github.com>
2025-12-20 18:56:40 +0530

    updated

--- index.js
+++ index.js
@@ -16,9 +16,26 @@ app.get("/", (req, res) => {
   })
 })
 
+app.get('/file/:filename', (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render('show',{filename: req.params.filename, filedata: filedata})
+  })
+})
+
 app.post('/create', (req, res) => {
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.detail, (err) => {
     res.redirect("/")
+  })
+})
+
+app.get("/edit/:filename", (req, res) => {
+  res.render("edit", {filename : req.params.filename})
+})
+
+app.post("/edit", (req, res) => {
+  console.log(req.body)
+  fs.rename(`./files/${req.body.prev}`, `./files/${req.body.new}.txt`, (err) => {
+    res.redirect('/')
   })
 })
 
